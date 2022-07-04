7740ffc35402e6305624d150167ea1a944368944
gabrielrios27 <juangrios27@gmail.com>
2022-07-04 12:35:35 -0300

    Feat: Crea componente admin-layout
    Este sera el container del header heneral y el navbar lateral

---
+++ src/app/core/layouts/containers/admin-layout/admin-layout.component.spec.ts
@@ -0,0 +1,50 @@
+import { Component, DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AdminLayoutComponent } from './admin-layout.component';
+
+@Component({
+    template: `
+        <app-admin-layout [someInput]="someInput" (someFunction)="someFunction($event)"></app-admin-layout>
+    `,
+})
+class TestHostComponent {
+    // someInput = 1;
+    // someFunction(event: Event) {}
+}
+
+describe('AdminLayoutComponent', () => {
+    let fixture: ComponentFixture<TestHostComponent>;
+    let hostComponent: TestHostComponent;
+    let hostComponentDE: DebugElement;
+    let hostComponentNE: Element;
+
+    let component: AdminLayoutComponent;
+    let componentDE: DebugElement;
+    let componentNE: Element;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestHostComponent, AdminLayoutComponent],
+            imports: [NoopAnimationsModule],
+            providers: [],
+            schemas: [NO_ERRORS_SCHEMA],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(TestHostComponent);
+        hostComponent = fixture.componentInstance;
+        hostComponentDE = fixture.debugElement;
+        hostComponentNE = hostComponentDE.nativeElement;
+
+        componentDE = hostComponentDE.children[0];
+        component = componentDE.componentInstance;
+        componentNE = componentDE.nativeElement;
+
+        fixture.detectChanges();
+    });
+
+    it('should display the component', () => {
+        expect(hostComponentNE.querySelector('app-admin-layout')).toEqual(jasmine.anything());
+    });
+});
---
+++ src/app/core/layouts/containers/admin-layout/admin-layout.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-layout',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './admin-layout.component.html',
+  styleUrls: ['admin-layout.component.scss'],
+})
+export class AdminLayoutComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
