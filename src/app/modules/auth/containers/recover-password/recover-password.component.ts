a126ee17ff78cca09fd97830173886d1dcfc8e95
MatiSal <salinasmatias100@gmail.com>
2022-07-07 12:27:20 -0300

    Refactor: Cambia maquetacion de la pantalla
    cambia la pantalla en la cual se cambia contraseña y redirecciona al dashboar

--- src/app/modules/auth/containers/recover-password/recover-password.component.ts
+++ src/app/modules/auth/containers/recover-password/recover-password.component.ts
@@ -1,15 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
 
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
 
 @Component({
   selector: 'app-recover-password',
@@ -18,23 +8,16 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class RecoverPasswordComponent implements OnInit {
 
-  flag: boolean = false
+  constructor() {
 
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  matcher = new MyErrorStateMatcher();
-  constructor() { 
-    
   }
-  
+
 
   ngOnInit(): void {
   }
 
-  submit(){
-    const img = document.getElementById("img")
-    this.flag = true
-    console.log(this.emailFormControl.value)
-    img?.style.setProperty('margin-top', '267px');
+  confirm(){
+    
   }
 
 }
