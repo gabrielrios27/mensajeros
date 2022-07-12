7d9e64aa6697072d9c6ef6d374dd0a38916ecd39
MatiSal <salinasmatias100@gmail.com>
2022-07-12 14:06:02 -0300

    Refactor: agrega mustmatch en input y mat-error para detectar errores
    mustMatch comprueba que los inputs son iguales y mat-error muestra los errores en input por pantalla

--- src/app/modules/auth/containers/recover-password/recover-password.component.ts
+++ src/app/modules/auth/containers/recover-password/recover-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recover-password',
@@ -7,17 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recover-password.component.scss']
 })
 export class RecoverPasswordComponent implements OnInit {
+  flag: boolean = false
+  formg!: FormGroup;
 
-  constructor() {
-
+  constructor(private router: Router, private fb: FormBuilder ) {
+    this.formg = this.fb.group({
+      password:  ['', Validators.minLength(8)],
+      cPassword:  ['', Validators.required]
+    },
+    {
+      validators:this.mustMatch('password','cPassword')
+    }
+    )
   }
 
 
   ngOnInit(): void {
+    console.log(this.formg.controls.errors)
   }
 
   confirm(){
-    
+    this.router.navigate(["/dashboar"])
   }
+
+  get f(){
+    return this.formg.controls;
+  }
+
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+
+        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            // return if another validator has already found an error on the matchingControl
+            return;
+        }
+
+        // set error on matchingControl if validation fails
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+}
 
 }
