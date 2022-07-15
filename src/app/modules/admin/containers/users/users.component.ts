6a597effe2bb917eb8f032c13e89d8526ad2c653
MatiSal <salinasmatias100@gmail.com>
2022-07-15 19:04:42 -0300

    Feat: crea maquetacion de la pantalla usuario
    el admin visualizara los usuarios creados, ademas de crearlos modificarlos y eliminarlos

--- src/app/modules/admin/containers/users/users.component.ts
+++ src/app/modules/admin/containers/users/users.component.ts
@@ -1,4 +1,22 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Routes, Router, ActivatedRoute } from '@angular/router';
+import { DataService } from '../../services/data.service';
+
+
+
+export interface PeriodicElement {
+  usuario: string;
+  position: number;
+  centroAsignado: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, usuario: 'Hydrogen ramon', centroAsignado: 1.0079 },
+  {position: 2, usuario: 'Helium pepe', centroAsignado: 4.0026 },
+  {position: 3, usuario: 'Lithium tartamudo', centroAsignado: 6.941 },
+  {position: 4, usuario: 'Beryllium gege', centroAsignado: 9.0122 },
+];
 
 @Component({
   selector: 'app-users',
@@ -7,6 +25,27 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+  displayedColumns: string[] = [ 'usuario', 'centroAsignado','acciones'];
+  dataSource = ELEMENT_DATA;
+  
+  constructor(private router: Router, private _snackBar: MatSnackBar,private route: ActivatedRoute, public data: DataService) {}
+
+  openSnackBar() {
+    this._snackBar.open("Usuario creado", "cerrar");
+  }
+
+  ngOnInit() {
+    this.mostrar()
+  }
+
+  create(){
+    this.router.navigate(['admin/dashboard/usuarios/create-user']);
+  }
+
+  mostrar(){
+    let flag = true
+    if(flag){
+      this.openSnackBar;
+    }
+  }
 }
