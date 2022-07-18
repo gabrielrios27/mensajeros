f240ec330accb5c19207f97e1570c72d60f7e41e
Juan Gabriel Rios D'Amico <juangrios27@gmail.com>
2022-07-18 16:00:39 -0300

    Feat: Crea servicio para peticiones http
    estas peticiones seran usadas en ejes, usuarios, y centros.

--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class AdminService {
-    constructor() {}
+  api_key: string = '';
+  baseUrl: string = 'https://mensajeros-back-martin.herokuapp.com';
+  headers = new HttpHeaders().set(
+    'Authorization',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTY3OTEzYWJlMTU0MTY5ZWE5ZDg1ZTNlOGEzZTdkYSIsInN1YiI6IjYyMTU0ZWRhMGU0ZmM4MDA0NDExNjZlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8-i63xqhXGI5bCPXp0dWpPktcxIJt_CUToTH5Sneyc8'
+  );
 
-    getAdmin$(): Observable<{}> {
-        return of({});
-    }
+  constructor(private _http: HttpClient) {}
 
+  getAxes(): Observable<any> {
+    return this._http.get<any>(this.baseUrl, {
+      headers: this.headers,
+    });
+  }
 }
