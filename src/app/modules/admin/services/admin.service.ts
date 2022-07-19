6357f87fb31ad0ca4d3dd1b7efebcd9a8af2d676
MatiSal <salinasmatias100@gmail.com>
2022-07-19 15:20:09 -0300

    Refactor:  crea metodo que utiliza endpoint centros

--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Centro } from '../models/centro';
 
 @Injectable()
 export class AdminService {
-    constructor() {}
+    api_key: string = ''
+    baseUrl: string =  "https://mensajeros-back-martin.herokuapp.com/"
+
+    constructor(private _http: HttpClient) {}
 
     getAdmin$(): Observable<{}> {
         return of({});
+    }
+
+    getCentro(): Observable<any>{
+        //let params = new HttpParams().set('api_key', this.api_Key);
+        return this._http.get<Centro[]>(this.baseUrl + 'centros')
     }
 
 }
