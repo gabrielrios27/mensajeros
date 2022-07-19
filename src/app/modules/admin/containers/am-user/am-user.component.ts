d71d1e5c3fd6d968d9b5735766700e8ffb8b6271
MatiSal <salinasmatias100@gmail.com>
2022-07-19 10:54:14 -0300

    Refactor: agrega ngmodel para el edit
    ayuda a visualizar los datos que vinen desde el otro componente user

--- src/app/modules/admin/containers/am-user/am-user.component.ts
+++ src/app/modules/admin/containers/am-user/am-user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Users } from '../../models/users';
 
 @Component({
   selector: 'app-am-user',
@@ -11,6 +12,10 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class AmUserComponent implements OnInit {
   
   formUpEdit: FormGroup;
+  nombre: any
+  email: any
+  contrasena: any
+  centroAsignado: any
 
   constructor(private router: Router, private data:DataService, private fb:FormBuilder) {
     this.formUpEdit = fb.group({
@@ -22,12 +27,21 @@ export class AmUserComponent implements OnInit {
 
   
   ngOnInit(): void {
+    this.nombre = this.data.user?.nombre
+    this.contrasena = this.data.user?.contrasena
+    this.email = this.data.user?.email
+    this.centroAsignado = this.data.user?.centroAsignado
   }
 
   confirm(){
     this.router.navigate(['admin/dashboard/usuarios']);
     this.data.flag = true
     this.data.nombreUsuario = "pepe"
+  }
+
+  edit(){
+    
+    
   }
 
 }
