30ae726bec5c5df7d9e53725f1e7fc63e9d33098
MatiSal <salinasmatias100@gmail.com>
2022-07-19 15:19:09 -0300

    Feat: crea interface de centro

---
+++ src/app/modules/admin/models/centro.ts
@@ -0,0 +1,13 @@
+export interface Centro {
+    id: number
+    nombre: string
+    usuario:{
+        byEmail: string
+        contra: string
+        email: string
+        id: number
+        nombre: string
+        rolNombre: string
+        token: string
+    }
+}
