ad47d4bf955777397ce678da1f30edfff256d831
MatiSal <salinasmatias100@gmail.com>
2022-07-20 16:12:17 -0300

    Refactor: agrega objeto centros para pruebas

--- src/app/modules/admin/containers/users/users.component.ts
+++ src/app/modules/admin/containers/users/users.component.ts
@@ -4,6 +4,7 @@ import { Routes, Router, ActivatedRoute } from '@angular/router';
 import { DataService } from '../../services/data.service';
 import { Users } from '../../models/users';
 import { Form } from '@angular/forms';
+import { Centro } from '../../models/centro';
 
 
 
@@ -14,10 +15,31 @@ export interface PeriodicElement {
 }
 
 const list: Users[] = [
-  {nombre: 'pepe',contrasena:"1234", email:"pepe@gmail.com",centroAsignado: "colibri" },
-  {nombre: 'pepes',contrasena:"1234", email:"pepe@gmail.com",centroAsignado: "colibri"  },
-  {nombre: 'pepei',contrasena:"1234", email:"pepe@gmail.com",centroAsignado: "colibri"  },
+  { nombre: 'Facundo Gonzalez', contrasena: "1234", email: "pepe@gmail.com", id: 1 },
+  { nombre: 'pepes', contrasena: "1234", email: "pepe@gmail.com", id: 2 },
+  { nombre: 'pepei', contrasena: "1234", email: "pepe@gmail.com", id: 3 },
 ];
+
+const centros: Centro[] = [
+  {
+  id: 1,
+  nombre: "Colibries",
+  usuario:  {
+    id: 1,
+    nombre: "Facundo Gonzalez",
+    email: "facugonzalez@gmail.com"
+  }
+},
+{
+  id: 2,
+    nombre: "La Balsa",
+  usuario: {
+    id: 1,
+      nombre: "pepes",
+      email: "facugonzalez@gmail.com"
+  }
+}
+]
 
 @Component({
   selector: 'app-users',
@@ -26,11 +48,13 @@ const list: Users[] = [
   styleUrls: ['users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-  displayedColumns: string[] = [ 'id', 'usuario', 'centroAsignado','acciones'];
+  displayedColumns: string[] = ['id', 'usuario', 'centroAsignado', 'acciones'];
   dataSource = list;
   usuario: any
-  
-  constructor(private router: Router,private route: ActivatedRoute, public data: DataService) {}
+  //centro: Array<Centro> = new Array
+  centros = centros
+
+  constructor(private router: Router, private route: ActivatedRoute, public data: DataService) { }
 
 
   ngOnInit() {
@@ -39,41 +63,50 @@ export class UsersComponent implements OnInit {
 
   busca(e: string) {
 
-    if(e.toLocaleLowerCase() == ''){
+    if (e.toLocaleLowerCase() == '') {
       this.ngOnInit()
     }
-    else{
-      this.dataSource = list.filter(res => { 
+    else {
+      this.dataSource = list.filter(res => {
         return res.nombre.toLowerCase().match(this.usuario.toLowerCase())
-    })
-    console.log(this.dataSource)
+      })
+      console.log(this.dataSource)
     }
-    
+
   }
 
-  create(){
+  create() {
     this.router.navigate(['admin/dashboard/usuarios/create-user']);
   }
 
-  edit(user: Users){
+  edit(user: Users) {
     this.router.navigate(['admin/dashboard/usuarios/create-user'])
     this.data.user = user
   }
 
-  delete(user: Users){
-    for(let i of this.dataSource){
-      if(i.nombre === user.nombre){
-        this.dataSource.splice(this.dataSource.indexOf(i),1)
+  delete(user: Users) {
+    for (let i of this.dataSource) {
+      if (i.nombre === user.nombre) {
+        this.dataSource.splice(this.dataSource.indexOf(i), 1)
       }
     }
   }
 
+  getCentro(user: Users): any {
+    for (let centro of centros) {
+      if (user.nombre == centro.usuario.nombre) {
+        return centro.nombre
+      }
+    }
 
-  close(){
-    if(this.data.user !=null){
+  }
+
+
+  close() {
+    if (this.data.user != null) {
       this.dataSource.push(this.data.user)
     }
-    
+
     this.data.flag = false
   }
 }
