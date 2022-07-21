8ea75aec9fa4bd10bd3b563023488f825c4121b1
MatiSal <salinasmatias100@gmail.com>
2022-07-21 13:24:28 -0300

    Feat: crea maquetacion de alta y modificacion de centros

--- src/app/modules/admin/services/data.service.ts
+++ src/app/modules/admin/services/data.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Users } from '../models/users';
+import { Centro } from '../models/centro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   nombreUsuario: string =''
+  nombreCentro:string=''
   flag: boolean = false
+  editar: boolean = false
   user?: Users
+  center?: Centro
   constructor() { }
 }
