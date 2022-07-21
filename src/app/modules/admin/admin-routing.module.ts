597f063f7ecbb244c3b4d3f6bdf971d9ecc94602
gabrielrios27 <juangrios27@gmail.com>
2022-07-21 12:54:55 -0300

    Feat: Crea funciones para obtener eje con id
    se crea en service y en add-axe

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -51,6 +51,11 @@ export const ROUTES: Routes = [
     pathMatch: 'full',
   },
   {
+    path: 'ejes/agregar-eje/:id',
+    canActivate: [],
+    component: adminContainers.AddAxesComponent,
+  },
+  {
     path: 'reportes',
     canActivate: [],
     component: adminContainers.ReportsComponent,
--- src/app/modules/admin/containers/add-axes/add-axes.component.ts
+++ src/app/modules/admin/containers/add-axes/add-axes.component.ts
@@ -6,8 +6,25 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { axes } from '../../models';
+import { AdminService } from '../../services';
 
+const ELEMENT_DATA: axes[] = [
+  {
+    id: 123,
+    nombre: 'Educacion',
+  },
+  {
+    id: 34,
+    nombre: 'Salud',
+  },
+  {
+    id: 454,
+    nombre: 'Eje 3',
+  },
+];
 @Component({
   selector: 'app-add-axes',
   templateUrl: './add-axes.component.html',
@@ -28,9 +45,25 @@ export class AddAxesComponent implements OnInit {
     'Centro la Balsa',
   ];
   invalidForm: boolean = false;
-  constructor(private fb: FormBuilder, private router: Router) {}
+  idAxe: number;
+  // suscripciones
+  onDestroy$: Subject<boolean> = new Subject();
+  axeById: axes;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private rutaActiva: ActivatedRoute,
+    private _adminSvc: AdminService
+  ) {
+    this.idAxe = 0;
+    this.axeById = {} as axes;
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.idAxe = this.getIdFromRute();
+    console.log('id ruta:' + this.idAxe);
+    this.completeInputWithAxe(this.idAxe);
+  }
   onConfirm() {
     if (this.newAxe.invalid) {
       this.invalidForm = true;
@@ -49,5 +82,33 @@ export class AddAxesComponent implements OnInit {
   }
   setAxeLocStg(data: boolean) {
     localStorage.setItem('isNewAxe', JSON.stringify(data));
+  }
+  getIdFromRute(): number {
+    let idToShow;
+    this.rutaActiva.paramMap
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((params: ParamMap) => {
+        idToShow = params.get('id');
+      });
+    return Number(idToShow);
+  }
+  getAxeById(id: number) {
+    this._adminSvc.getAxeWithId(id).subscribe({
+      next: (data: axes) => {
+        this.axeById = data;
+        console.log('el axeById es:' + this.axeById);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('Request trending complete');
+      },
+    });
+  }
+  completeInputWithAxe(id: number) {
+    if (this.idAxe !== 0) {
+      this.getAxeById(id);
+    }
   }
 }
