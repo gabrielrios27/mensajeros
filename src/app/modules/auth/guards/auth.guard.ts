03657f145eb9c5c17fd9465d0f99f1f5fa93b83e
MatiSal <salinasmatias100@gmail.com>
2022-07-22 11:36:56 -0300

    Feat: crea metodo para controlar login

--- src/app/modules/auth/guards/auth.guard.ts
+++ src/app/modules/auth/guards/auth.guard.ts
@@ -1,10 +1,18 @@
+
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, pipe } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import {take, map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+    constructor(private loginService: AuthService){}
     canActivate(): Observable<boolean> {
+        return this.loginService.isLogged.pipe(
+            take(1),
+            map((islogged : boolean) => !islogged)
+        )
         return of(true);
     }
 }
