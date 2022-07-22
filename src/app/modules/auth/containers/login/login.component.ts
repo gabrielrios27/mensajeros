0f345a105c257f8ba5e59976a248a6589cc5d487
MatiSal <salinasmatias100@gmail.com>
2022-07-22 11:30:22 -0300

    Feat: agrega servicio de login y formgroup
    form group para controlar las interacciones con inputs y detectar errores

--- src/app/modules/auth/containers/login/login.component.ts
+++ src/app/modules/auth/containers/login/login.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +10,46 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+    loginForm: FormGroup
+    flag: boolean = false
+    errorMessage: any;
+
+    constructor(public serviceLogin: AuthService, private fb: FormBuilder, private router: Router,) {
+        this.loginForm = this.fb.group({
+            email: ['', Validators.required],
+            contrasena: ['', Validators.required]
+        })
+    }
+
+
+    ngOnInit() {
+
+    }
+
+    setLocalStorage(data: any) {
+        if (data) {
+            localStorage.setItem('Usuario', JSON.stringify(data))
+        }
+    }
+
+    onLogin(): any {
+        const from = this.loginForm.value
+        this.serviceLogin.loginByEmail(from).subscribe(
+            (data) => {
+                if (data) {
+                    console.log(data)
+                    this.setLocalStorage(data.token)
+                    this.router.navigate(['admin/dashboard/home'])
+                }
+            },
+            (error) => {
+                console.error("error", error.status)
+                if (error.status == 401) {
+                    this.errorMessage = error.status
+                }
+            }
+        )
+        this.flag = true
+    }
+
 }
