2143a70b136f6b162ec3433cce892d31b15e6f72
MatiSal <salinasmatias100@gmail.com>
2022-07-22 11:32:04 -0300

    Feat: crea interface login y response
    login para enviar los datos solicitados y response recibe la respuesta del back

---
+++ src/app/modules/auth/models/response.ts
@@ -0,0 +1,12 @@
+export interface Response {
+
+    authorities: [
+        {
+            empty: true,
+            present: true
+        }
+    ],
+    bearer: string,
+    email: string,
+    token: string
+}
