c6c9c3d3db97d81494c3760e8a61d004a3670b3b
MatiSal <salinasmatias100@gmail.com>
2022-07-25 12:43:16 -0300

    Refactor: agrega otro control de errores

--- src/app/modules/auth/containers/login/login.component.ts
+++ src/app/modules/auth/containers/login/login.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
@@ -12,9 +12,10 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
     loginForm: FormGroup
     flag: boolean = false
+    flag2: boolean = false
     errorMessage: any;
 
-    constructor(public serviceLogin: AuthService, private fb: FormBuilder, private router: Router,) {
+    constructor(public serviceLogin: AuthService, private fb: FormBuilder, private router: Router, private cdr : ChangeDetectorRef) {
         this.loginForm = this.fb.group({
             email: ['', Validators.required],
             contrasena: ['', Validators.required]
@@ -37,19 +38,28 @@ export class LoginComponent implements OnInit {
         this.serviceLogin.loginByEmail(from).subscribe(
             (data) => {
                 if (data) {
+                    setTimeout(() => this.cdr.detectChanges())
                     console.log(data)
                     this.setLocalStorage(data.token)
                     this.router.navigate(['admin/dashboard/home'])
+                    
                 }
             },
             (error) => {
                 console.error("error", error.status)
                 if (error.status == 401) {
                     this.errorMessage = error.status
+                    setTimeout(() => this.cdr.detectChanges())
+                    this.flag = true
                 }
+                else{
+                    this.errorMessage = error
+                    setTimeout(() => this.cdr.detectChanges())
+                    this.flag2 = true
+                }
+                
             }
         )
-        this.flag = true
     }
 
 }
