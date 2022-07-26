83e7656b7dc17809d431eb97ef7514f0a311dec8
MatiSal <salinasmatias100@gmail.com>
2022-07-26 19:28:42 -0300

    Feat: agrega atributo zona

--- src/app/modules/admin/models/centro.ts
+++ src/app/modules/admin/models/centro.ts
@@ -1,6 +1,7 @@
 import { Users } from './users';
 export interface Centro {
     id: number,
-        nombre: string
-        usuario: Users
+    nombre: string
+        usuario?: Users
+    zona: string
 }
