ef4523496fb31268e8f008415773bb57e9e9e850
MatiSal <salinasmatias100@gmail.com>
2022-07-27 10:09:53 -0300

    Feat: agrega metodos agregar y editar centro

--- src/app/modules/admin/containers/add-mod-center/add-mod-center.component.ts
+++ src/app/modules/admin/containers/add-mod-center/add-mod-center.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
+import { Centro } from '../../models/centro';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-add-mod-center',
@@ -13,7 +15,7 @@ export class AddModCenterComponent implements OnInit {
   nombre: any
   Zona: any
 
-  constructor(private router: Router, public data:DataService, private fb:FormBuilder) {
+  constructor(private router: Router, public data:DataService, private fb:FormBuilder, private admin: AdminService, private cdr: ChangeDetectorRef) {
     this.formUpEdit = fb.group({
       nombre: ['', Validators.required],
       zona: ['',Validators.required]
@@ -28,15 +30,45 @@ export class AddModCenterComponent implements OnInit {
   confirm(){
     this.router.navigate(['admin/dashboard/centros']);
     console.log(this.formUpEdit.value)
+    this.addCenter(this.formUpEdit.value)
     this.data.center = this.formUpEdit.value
     this.data.nombreCentro = this.formUpEdit.value.nombre
     this.formUpEdit.reset()
   }
 
+  addCenter(center: Centro){
+    this.admin.addCenter(this.formUpEdit.value).subscribe({
+      next: data =>{
+        setTimeout(() => this.cdr.detectChanges());
+        console.log(data)
+      },
+      error: err =>{
+        setTimeout(() => this.cdr.detectChanges());
+        console.log(err)
+      }
+    })
+  }
+
   editar(){
     this.router.navigate(['admin/dashboard/centros']);
-    this.data.flag = true
-    this.data.editar = true
+    let id = this.data.center?.id
+    this.editCenter(id)
+    this.formUpEdit.reset()
+  }
+
+  editCenter(id: any){
+    this.admin.editCenter(this.formUpEdit.value,id).subscribe({
+      next: data =>{
+        setTimeout(() => this.cdr.detectChanges());
+        console.log(data)
+        this.data.flag = true
+        this.data.editar = true
+      },
+      error: err =>{
+        setTimeout(() => this.cdr.detectChanges());
+        console.log(err)
+      }
+    })
   }
 
 }
