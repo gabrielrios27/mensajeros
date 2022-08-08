7825f427418d3ab6382fabd83f759b2f94d55280
MatiSal <salinasmatias100@gmail.com>
2022-08-08 18:17:44 -0300

    Feat: crea componente crear reporte

---
+++ src/app/modules/admin/containers/add-mod-report/add-mod-report.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-add-mod-report',
+  templateUrl: './add-mod-report.component.html',
+  styleUrls: ['./add-mod-report.component.scss']
+})
+export class AddModReportComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
