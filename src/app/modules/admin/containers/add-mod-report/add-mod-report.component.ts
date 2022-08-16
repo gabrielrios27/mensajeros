fa47bf52204280c883f68274b7f2fe5a5f83823b
MatiSal <salinasmatias100@gmail.com>
2022-08-16 12:31:44 -0300

    Feat: agrega modal de salida y mat-errors

--- src/app/modules/admin/containers/add-mod-report/add-mod-report.component.ts
+++ src/app/modules/admin/containers/add-mod-report/add-mod-report.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Subject } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 
@@ -9,19 +11,34 @@ import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 })
 export class AddModReportComponent implements OnInit {
 
+  nombre: any
   variables: any;
   eje: any
-  ejes: Array<any> = ["hola","pepe","jose"]
+  ejes: Array<any> = ["hola", "pepe", "jose"]
   arrayc: Array<number> = [1]
+// para modal de advertencia
+  flagAddEdit: boolean = false;
+  showDialog = false;
+  subject = new Subject<boolean>();
+//
+  formAdd: FormGroup;
 
-  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
+
+  constructor(private fb: FormBuilder,) {
+    this.formAdd = fb.group({
+      nombre: ['', Validators.required],
+      centros: ['', Validators.required],
+      desde: ['', Validators.required],
+      hasta: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
-    
+    this.setFlagAddEdit(false);
   }
 
   createEje() {
-   this.arrayc.push(1)
+    this.arrayc.push(1)
   }
 
   capturarVariables(e: any) {
@@ -35,4 +52,24 @@ export class AddModReportComponent implements OnInit {
   removeVariable(variable: any) {
     this.variables = this.variables.filter((res: any) => res !== variable);
   }
+// Para modal de advertencia de cambio de pantalla
+  setFlagAddEdit(value: boolean) {
+    this.flagAddEdit = value;
+    localStorage.setItem('flagAddEdit', JSON.stringify(this.flagAddEdit));
+  }
+  onSelection($event: any) {
+    console.log($event);
+    this.showDialog = false;
+    if ($event === 'ok') {
+      this.subject.next(true);
+      this.setFlagAddEdit(false);
+    } else {
+      this.subject.next(false);
+    }
+  }
+  openDialog() {
+    console.log('opn dialog');
+    this.showDialog = true;
+  }
+//
 }
