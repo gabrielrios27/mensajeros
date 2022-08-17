0b81cbfb50e166d146d9ca2de44445de27865584
MatiSal <salinasmatias100@gmail.com>
2022-08-17 17:58:30 -0300

    Feat: agrega funcion para cerrar modal

--- src/app/modules/admin/containers/preview-report/preview-report.component.ts
+++ src/app/modules/admin/containers/preview-report/preview-report.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-preview-report',
@@ -6,17 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview-report.component.scss']
 })
 export class PreviewReportComponent implements OnInit {
+  @Input() oldname = "";
+  @Output() close = new EventEmitter<string>();
+  newname = "";
+  
 
-  centros: Array<any> = ["la balsa","h.san jose","casa 21"]
+  centros: Array<any> = ["la balsa", "h.san jose", "casa 21"]
   desde: string = "01/07/2022"
   hasta: string = "31/12/2022"
-  name:string = "123123123"
-  variables: Array<any> = ["variable","variable","variable","variable"]
+  name: string = "123123123"
+  variables: Array<any> = ["variable", "variable", "variable", "variable"]
   ejes: Array<any> = ["seguridad alimentaria", "acompañamiento ", "psicologia"]
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // copy all inputs to avoid polluting them
+    this.newname = this.oldname;
+    
   }
 
+  ok() {
+    this.close.emit(this.newname);
+  }
+
+  cancel() {
+    this.close.emit(undefined);
+  }
 }
