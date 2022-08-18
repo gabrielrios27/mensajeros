90d5e2e95461a11d1b91025f54d5acacc28d14c2
MatiSal <salinasmatias100@gmail.com>
2022-08-18 09:45:48 -0300

    Feat: agrega metodo setea localstorage, enviar, confirmar y cancelar

--- src/app/modules/admin/containers/preview-report/preview-report.component.ts
+++ src/app/modules/admin/containers/preview-report/preview-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { flag } from '../../models/admin.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-preview-report',
@@ -9,7 +11,7 @@ export class PreviewReportComponent implements OnInit {
   @Input() oldname = "";
   @Output() close = new EventEmitter<string>();
   newname = "";
-  
+  flag: boolean = false
 
   centros: Array<any> = ["la balsa", "h.san jose", "casa 21"]
   desde: string = "01/07/2022"
@@ -18,7 +20,7 @@ export class PreviewReportComponent implements OnInit {
   variables: Array<any> = ["variable", "variable", "variable", "variable"]
   ejes: Array<any> = ["seguridad alimentaria", "acompañamiento ", "psicologia"]
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     // copy all inputs to avoid polluting them
@@ -32,5 +34,23 @@ export class PreviewReportComponent implements OnInit {
 
   cancel() {
     this.close.emit(undefined);
+  }
+  backAddReport(){
+    this.router.navigate(['admin/dashboard/reportes/creacion-de-reportes/add-mod-report'])
+  }
+
+  confirm(){
+    this.flag = true
+  }
+
+  enviar(){
+    this.setUserLocStg("algo", true)
+    this.flag = false
+    this.router.navigate(['/admin/dashboard/reportes/creacion-de-reportes']);
+  }
+
+  setUserLocStg(data: string, isNewUser: boolean) {
+    localStorage.setItem('newOrEditedReport', data);
+    localStorage.setItem('isNewReport', JSON.stringify(isNewUser));
   }
 }
