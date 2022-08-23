1e0ddf110d7caea070be7d8a6f33e0ac0d98ed9a
MatiSal <salinasmatias100@gmail.com>
2022-08-22 22:15:37 -0300

    Feat: agrega funciones para menejos de datos
    agrego input report
    
    funciones para obter centros y mostrar nombre de centros seleccionados
    
    funcion getdatafromrute para obtener datos de la url

--- src/app/modules/admin/containers/preview-report/preview-report.component.ts
+++ src/app/modules/admin/containers/preview-report/preview-report.component.ts
@@ -1,5 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { axes, variable } from '../../models';
+import { AdminService } from '../../services';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-preview-report',
@@ -10,22 +13,79 @@ export class PreviewReportComponent implements OnInit {
   @Input() oldname = "";
   @Output() close = new EventEmitter<string>();
   newname = "";
+  @Input() report: any
+  name: any
+  until: any
+  since: any
+  reportsd: any
+
   flag: boolean = false
+  listOfAxes: Array<any> = []
+  listOfVariables: Array<any> = []
+  centers: Array<any> = []
+  centers2: any
+  centerSelects: Array<any> = []
 
-  centros: Array<any> = ["la balsa", "h.san jose", "casa 21"]
-  desde: string = "01/07/2022"
-  hasta: string = "31/12/2022"
-  name: string = "123123123"
-  variables: Array<any> = ["variable", "variable", "variable", "variable"]
-  ejes: Array<any> = ["seguridad alimentaria", "acompañamiento ", "psicologia"]
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private admin: AdminService, private routeActiva: ActivatedRoute,
+    private cdr: ChangeDetectorRef, public data: DataService) { }
 
   ngOnInit() {
     // copy all inputs to avoid polluting them
     this.newname = this.oldname;
+    console.log(this.report)
+    this.getCenters()
+    this.getDataFromRute()
+  }
+
+  getCenters() {
+    this.admin.getCentros().subscribe({
+      next: (data) => {
+        setTimeout(() => this.cdr.detectChanges());
+        this.centers = data;
+        this.centerSelect()
+        this.center2()
+        console.log(data);
+      },
+      error: (err) => {
+        setTimeout(() => this.cdr.detectChanges());
+        console.log(err);
+      },
+    });
+  }
+
+  centerSelect() {
+    if(this.data.arrayCenters == null)
+    for (let item of this.centers) {
+      for (let c of this.report.centros) {
+        if (item.id == c) {
+          console.log(item)
+          this.centerSelects.push(item)
+        }
+      }
+    }
     
   }
+  center2(){
+    console.log(this.centers2)
+      for (let item of this.centers) {
+      for (let ce of this.data.arrayCenters) {
+        console.log(ce)
+        if (item.id == ce) {
+          console.log(item)
+          this.centerSelects.push(item)
+        }
+      }
+    }
+  }
+
+  getDataFromRute() {
+    this.routeActiva.paramMap.subscribe((params: ParamMap) => {
+      this.name = params.get("nombre");
+      this.since = params.get("desde");
+      this.until = params.get("hasta");
+    });
+  }
+
 
   ok() {
     this.close.emit(this.newname);
@@ -34,15 +94,15 @@ export class PreviewReportComponent implements OnInit {
   cancel() {
     this.close.emit(undefined);
   }
-  backAddReport(){
+  backAddReport() {
     this.router.navigate(['admin/dashboard/reportes/creacion-de-reportes/add-mod-report'])
   }
 
-  confirm(){
+  confirm() {
     this.flag = true
   }
 
-  enviar(){
+  enviar() {
     this.setUserLocStg("algo", true)
     this.flag = false
     this.router.navigate(['/admin/dashboard/reportes/creacion-de-reportes']);
