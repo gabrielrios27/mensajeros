8ce257013d3145d6e80ef2d5e112acd73cc736cc
Juan Gabriel Rios D'Amico <juangrios27@gmail.com>
2022-08-24 18:30:40 -0300

    Feat: agrega modal de esta apunto de eliminar reporte
    Si se da click a Volver a empezar se renderiza el pop up de advertencia

--- src/app/modules/user/containers/pending-reports/pending-reports.component.ts
+++ src/app/modules/user/containers/pending-reports/pending-reports.component.ts
@@ -7,47 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PendingReportsComponent implements OnInit {
   list: any = [
-    // {
-    //   numberReport: 'C00010',
-    //   center: 'Colibries',
-    //   period: '07/2021-12/2121',
-    //   limitDate: '02/01/2022',
-    //   lastAxeComplete: 2,
-    //   quantityOfAxes: 4,
-    // },
-    // {
-    //   numberReport: 'C00011',
-    //   center: 'La Balsa',
-    //   period: '07/2021-12/2121',
-    //   limitDate: '02/01/2022',
-    //   lastAxeComplete: 1,
-    //   quantityOfAxes: 8,
-    // },
-    // {
-    //   numberReport: 'C00012',
-    //   center: 'La Balsa',
-    //   period: '07/2021-12/2121',
-    //   limitDate: '02/01/2022',
-    //   lastAxeComplete: 0,
-    //   quantityOfAxes: 5,
-    // },
-    // {
-    //   numberReport: 'C00013',
-    //   center: 'Colibries',
-    //   period: '07/2021-12/2121',
-    //   limitDate: '02/01/2022',
-    //   lastAxeComplete: 0,
-    //   quantityOfAxes: 6,
-    // },
+    {
+      numberReport: 'C00010',
+      center: 'Colibries',
+      period: '07/2021-12/2121',
+      limitDate: '02/01/2022',
+      lastAxeComplete: 2,
+      quantityOfAxes: 4,
+    },
+    {
+      numberReport: 'C00011',
+      center: 'La Balsa',
+      period: '07/2021-12/2121',
+      limitDate: '02/01/2022',
+      lastAxeComplete: 1,
+      quantityOfAxes: 8,
+    },
+    {
+      numberReport: 'C00012',
+      center: 'La Balsa',
+      period: '07/2021-12/2121',
+      limitDate: '02/01/2022',
+      lastAxeComplete: 0,
+      quantityOfAxes: 5,
+    },
+    {
+      numberReport: 'C00013',
+      center: 'Colibries',
+      period: '07/2021-12/2121',
+      limitDate: '02/01/2022',
+      lastAxeComplete: 0,
+      quantityOfAxes: 6,
+    },
   ];
   reportToShow: any;
+  flagDeleteReport: boolean;
   constructor() {
     this.reportToShow = this.list[0];
+    this.flagDeleteReport = false;
   }
 
   ngOnInit(): void {}
   onStartReport() {}
   onClickReport(item: any) {
     this.reportToShow = item;
+  }
+  onDeleteReport(value: boolean) {
+    this.flagDeleteReport = value;
+    console.log('flagdelete:', this.flagDeleteReport);
   }
 }
