c60879e5b8ff34b01108d2fd11f6bd715a7d70d0
MatiSal <salinasmatias100@gmail.com>
2022-08-25 12:52:03 -0300

    Feat: agrega showModal. get centros y get axes
    Show modal muestra el modal de visualizar reporte

--- src/app/modules/admin/containers/reports/reports.component.ts
+++ src/app/modules/admin/containers/reports/reports.component.ts
@@ -7,8 +7,8 @@ import {
 import { Router } from '@angular/router';
 import { AdminService } from '../../services/admin.service';
 import { Report } from '../../models/report';
-import { element } from 'protractor';
-import { axes } from '../../models/admin.model';
+import { axes, variable } from '../../models/admin.model';
+import { Centro } from '../../models/centro';
 
 @Component({
   selector: 'app-reports',
@@ -40,19 +40,27 @@ export class ReportsComponent implements OnInit {
   showIt = false;
   arrayAxes: Array<axes> = [];
   //
+  listOfAxes: Array<axes> = []
+  listOfVariables: Array<variable> = []
 
   flag: boolean = false;
   reports: Array<Report> = [];
+  report: Report = {} as Report
+  centerSelects: Array<any> = []
+  centers: Array<Centro> = []
 
   constructor(
     private router: Router,
     private admin: AdminService,
     private cdr: ChangeDetectorRef
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.getReports();
     this.getUserLocalStorage();
+    this.getCenters()
+    this.getAxes()
+    this.getVariables()
   }
 
   busca(e: string) {
@@ -69,156 +77,227 @@ export class ReportsComponent implements OnInit {
 
   //visualizar reporte
   showModal(element: Report) {
+    
+    this.report = element
+    this.centerSelect()
     this.showIt = true;
+    console.log(element)
   }
 
-  closeModal(newName: string) {
+  closeModal() {
     this.showIt = false;
-    if (newName) this.name = newName;
+    this.centerSelects = []
   }
 
-  //
 
-  getReports() {
-    //this.currentPage = this.getPageLocalStorage();
-    this.admin.getResports().subscribe({
-      next: (data) => {
+  centerSelect() {
+    for (let item of this.centers) {
+      console.log(item.id)
+      for (let c of this.report.centros) {
+        console.log(c)
+        if (item.id == c) {
+          console.log(item)
+          this.centerSelects.push(item.nombre)
+        }
+      }
+    }
+    console.log(this.centerSelects)
+  }
+    //
+
+    getCenters() {
+      this.admin.getCentros().subscribe({
+        next: (data) => {
+          setTimeout(() => this.cdr.detectChanges());
+          this.centers = data;
+          console.log(data);
+        },
+        error: (err) => {
+          setTimeout(() => this.cdr.detectChanges());
+          console.log(err);
+        },
+      });
+    }
+
+    getReports() {
+      //this.currentPage = this.getPageLocalStorage();
+      this.admin.getResports().subscribe({
+        next: (data) => {
+          setTimeout(() => this.cdr.detectChanges());
+          this.reports = data;
+          this.pageToShow(this.currentPage, this.reports); //para paginación
+          console.log('reports', data);
+        },
+        error: (err) => {
+          setTimeout(() => this.cdr.detectChanges());
+          console.log(err);
+        },
+      });
+    }
+
+    getAxes() {
+      this.admin
+        .getAxes()
+        .subscribe({
+          next: (data: axes[]) => {
+            this.listOfAxes = data;
+            setTimeout(() => this.cdr.detectChanges());
+            console.log(this.listOfAxes);
+          },
+          error: (err) => {
+            console.log(err);
+            if (err.status === 401) {
+              this.router.navigate(['/auth']);
+            }
+          },
+          complete: () => {
+            console.log('Request get axes complete');
+          },
+        });
+    }
+  
+    getVariables(){
+      this.admin.getVariables().subscribe({ next: (data: variable[]) => {
+        this.listOfVariables = data;
         setTimeout(() => this.cdr.detectChanges());
-        this.reports = data;
-        this.pageToShow(this.currentPage, this.reports); //para paginación
-        console.log('reports', data);
+        console.log(this.listOfVariables);
       },
       error: (err) => {
-        setTimeout(() => this.cdr.detectChanges());
         console.log(err);
+        if (err.status === 401) {
+          this.router.navigate(['/auth']);
+        }
+      },
+      complete: () => {
+        console.log('Request get axes complete');
       },
     });
-  }
+    }
 
-  deleteReport() {
-    this.admin.deleteReport(this.idToDelete).subscribe({
-      next: (data) => {
-        setTimeout(() => this.cdr.detectChanges());
-        this.pageToShow(this.currentPage, this.reports); //para paginación
-        this.getReports();
-        this.close();
-        console.log('delete report', data);
-      },
-      error: (err) => {
-        setTimeout(() => this.cdr.detectChanges());
-        console.log(err);
-      },
-    });
-  }
+    deleteReport() {
+      this.admin.deleteReport(this.idToDelete).subscribe({
+        next: (data) => {
+          setTimeout(() => this.cdr.detectChanges());
+          this.pageToShow(this.currentPage, this.reports); //para paginación
+          this.getReports();
+          this.close();
+          console.log('delete report', data);
+        },
+        error: (err) => {
+          setTimeout(() => this.cdr.detectChanges());
+          console.log(err);
+        },
+      });
+    }
 
-  create() {
-    this.router.navigate([
-      'admin/dashboard/reportes/creacion-de-reportes/add-mod-report',
-    ]);
-  }
+    create() {
+      this.router.navigate([
+        'admin/dashboard/reportes/creacion-de-reportes/add-mod-report',
+      ]);
+    }
 
-  //para paginación----
-  pageToShow(page: number, list: Report[]) {
-    // this.setPageLocalStorage(page);
-    this.listLenght = list.length;
-    this.quantityOfPages = Math.ceil(this.listLenght / this.itemsPerPage);
-    this.listCurrentPage = [];
-    if (page <= 1) {
-      this.listCurrentPage = list.slice(0, 10);
-      this.reports = this.listCurrentPage;
-      this.initialItem = 1;
-      if (this.listLenght < this.itemsPerPage) {
-        this.finalItem = this.listLenght;
-      } else {
-        this.finalItem = 10;
+    //para paginación----
+    pageToShow(page: number, list: Report[]) {
+      // this.setPageLocalStorage(page);
+      this.listLenght = list.length;
+      this.quantityOfPages = Math.ceil(this.listLenght / this.itemsPerPage);
+      this.listCurrentPage = [];
+      if (page <= 1) {
+        this.listCurrentPage = list.slice(0, 10);
+        this.reports = this.listCurrentPage;
+        this.initialItem = 1;
+        if (this.listLenght < this.itemsPerPage) {
+          this.finalItem = this.listLenght;
+        } else {
+          this.finalItem = 10;
+        }
+      } else if (page > 1 && page < this.quantityOfPages) {
+        this.listCurrentPage = list.slice(
+          page * this.itemsPerPage - this.itemsPerPage,
+          page * this.itemsPerPage
+        );
+        this.reports = this.listCurrentPage;
+        this.initialItem = page * this.itemsPerPage - this.itemsPerPage + 1;
+        this.finalItem =
+          page * this.itemsPerPage -
+          this.itemsPerPage +
+          this.listCurrentPage.length;
+      } else if (page >= this.quantityOfPages) {
+        this.listCurrentPage = list.slice(
+          this.quantityOfPages * this.itemsPerPage - this.itemsPerPage
+        );
+        this.reports = this.listCurrentPage;
+        this.initialItem =
+          this.quantityOfPages * this.itemsPerPage - this.itemsPerPage + 1;
+        this.finalItem =
+          this.quantityOfPages * this.itemsPerPage -
+          this.itemsPerPage +
+          this.listCurrentPage.length;
       }
-    } else if (page > 1 && page < this.quantityOfPages) {
-      this.listCurrentPage = list.slice(
-        page * this.itemsPerPage - this.itemsPerPage,
-        page * this.itemsPerPage
-      );
-      this.reports = this.listCurrentPage;
-      this.initialItem = page * this.itemsPerPage - this.itemsPerPage + 1;
-      this.finalItem =
-        page * this.itemsPerPage -
-        this.itemsPerPage +
-        this.listCurrentPage.length;
-    } else if (page >= this.quantityOfPages) {
-      this.listCurrentPage = list.slice(
-        this.quantityOfPages * this.itemsPerPage - this.itemsPerPage
-      );
-      this.reports = this.listCurrentPage;
-      this.initialItem =
-        this.quantityOfPages * this.itemsPerPage - this.itemsPerPage + 1;
-      this.finalItem =
-        this.quantityOfPages * this.itemsPerPage -
-        this.itemsPerPage +
-        this.listCurrentPage.length;
     }
-  }
-  onClickBefore() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.pageToShow(this.currentPage, this.userListComplete);
-    } else {
-      this.currentPage = 1;
-      this.pageToShow(this.currentPage, this.userListComplete);
-    }
-  }
-  onClickAfter() {
-    if (this.currentPage < this.quantityOfPages) {
-      this.currentPage++;
-      this.pageToShow(this.currentPage, this.userListComplete);
-    } else {
-      this.currentPage = this.quantityOfPages;
-      this.pageToShow(this.currentPage, this.userListComplete);
-    }
-  }
-
-  onClickDelete(id: number) {
-    this.flagDelete = true;
-    this.idToDelete = id;
-  }
-
-  // deleteUser() {
-  //   this.flagDelete = false;
-  //   this.admin.deleteUser(this.idToDelete).subscribe({
-  //     next: (data: any) => {
-  //       setTimeout(() => this.cdr.detectChanges());
-  //       this.getUsers();
-  //     },
-  //     error: (err) => {
-  //       console.log(err);
-  //     },
-  //   });
-  // }
-  getUserLocalStorage() {
-    let newOrEditeduser = localStorage.getItem('newOrEditedReport');
-    if (newOrEditeduser) {
-      setTimeout(() => {
-        this.close();
-      }, 3000);
-    }
-
-    let isNewUserStr = localStorage.getItem('isNewReport');
-    let isNewUser;
-    if (isNewUserStr) {
-      isNewUser = JSON.parse(isNewUserStr);
-    }
-    if (newOrEditeduser) {
-      if (isNewUser) {
-        this.flagNew = true;
-        localStorage.removeItem('isNewReport');
+    onClickBefore() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        this.pageToShow(this.currentPage, this.userListComplete);
       } else {
-        this.flagEdited = true;
+        this.currentPage = 1;
+        this.pageToShow(this.currentPage, this.userListComplete);
       }
-      localStorage.removeItem('newOrEditedReport');
+    }
+    onClickAfter() {
+      if (this.currentPage < this.quantityOfPages) {
+        this.currentPage++;
+        this.pageToShow(this.currentPage, this.userListComplete);
+      } else {
+        this.currentPage = this.quantityOfPages;
+        this.pageToShow(this.currentPage, this.userListComplete);
+      }
+    }
+
+    onClickDelete(id: number) {
+      this.flagDelete = true;
+      this.idToDelete = id;
+    }
+
+    // deleteUser() {
+    //   this.flagDelete = false;
+    //   this.admin.deleteUser(this.idToDelete).subscribe({
+    //     next: (data: any) => {
+    //       setTimeout(() => this.cdr.detectChanges());
+    //       this.getUsers();
+    //     },
+    //     error: (err) => {
+    //       console.log(err);
+    //     },
+    //   });
+    // }
+    getUserLocalStorage() {
+      let newOrEditeduser = localStorage.getItem('newOrEditedReport');
+      if (newOrEditeduser) {
+        setTimeout(() => {
+          this.close();
+        }, 3000);
+      }
+
+      let isNewUserStr = localStorage.getItem('isNewReport');
+      let isNewUser;
+      if (isNewUserStr) {
+        isNewUser = JSON.parse(isNewUserStr);
+      }
+      if (newOrEditeduser) {
+        if (isNewUser) {
+          this.flagNew = true;
+          localStorage.removeItem('isNewReport');
+        } else {
+          this.flagEdited = true;
+        }
+        localStorage.removeItem('newOrEditedReport');
+      }
+    }
+
+    close() {
+      this.flagNew = false;
+      this.flagEdited = false;
+      this.flagDelete = false;
     }
   }
-
-  close() {
-    this.flagNew = false;
-    this.flagEdited = false;
-    this.flagDelete = false;
-  }
-}
