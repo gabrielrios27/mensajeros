49be677fb07bbd4f7cfe383ab56f7a277eedb7f9
MatiSal <salinasmatias100@gmail.com>
2022-08-25 17:39:52 -0300

    no message

--- src/app/modules/admin/models/report.ts
+++ src/app/modules/admin/models/report.ts
@@ -1,12 +1,12 @@
 import { variable } from './admin.model';
 import { Centro } from './centro';
 export interface Report {
-    centros: Array<Centro>,
+    centros: Array<any>,
     fechaCreacion: string
     fechaEntrega: string,
     id: any,
     nombre: string,
-    variables: Array<number>
+    variables: Array<any>
     periodoDesde : string
     periodoHasta : string
 }
