68aeb042a8d56aae56899c063bc5270d34c80b1e
MatiSal <salinasmatias100@gmail.com>
2022-08-26 13:45:38 -0300

    Feat: agrega funciones de get
    las cuales obtienen el reporte por id, axes, variables, centros

--- src/app/modules/admin/containers/add-mod-report/add-mod-report.component.ts
+++ src/app/modules/admin/containers/add-mod-report/add-mod-report.component.ts
@@ -1,31 +1,36 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Subject } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { getMaxListeners, report } from 'process';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { AdminService } from '../../services/admin.service';
 import { axes, variable } from '../../models/admin.model';
 import { Centro } from '../../models/centro';
 import { Report } from '../../models/report';
 import { DataService } from '../../services/data.service';
 
-
-
 @Component({
   selector: 'app-add-mod-report',
   templateUrl: './add-mod-report.component.html',
-  styleUrls: ['./add-mod-report.component.scss']
+  styleUrls: ['./add-mod-report.component.scss'],
 })
 export class AddModReportComponent implements OnInit {
-
-  deliverdate: any
-  nombre: any
+  deliverdate: any;
+  nombre: any;
+  desde: any;
+  hasta: any;
+  id: any
   variables: any;
-  eje: any
-  ejes: Array<any> = ["hola", "pepe", "jose"]
-  arrayc: Array<number> = [1]
-  arrayAxes : Array<any> = []
-  arrayVaribles :  Array<any> = []
+  eje: any;
+  ejes: Array<any> = ['hola', 'pepe', 'jose'];
+  arrayc: Array<number> = [1];
+  arrayAxes: Array<any> = [];
+  arrayVaribles: Array<any> = [];
+  report: Report = {} as Report
+  centerSelects: Array<any> = [];
+  axesSelects: Array<any> = [];
+  variablesSelects: Array<variable> = [];
+  listOfAxes: Array<axes> = [];
+  listOfVariables: Array<variable> = [];
   // para modal de advertencia
   flagAddEdit: boolean = false;
   showDialog = false;
@@ -33,15 +38,22 @@ export class AddModReportComponent implements OnInit {
   //
 
   formAdd: FormGroup;
-  listCenters: Array<Centro>= []
-  center: any
+  listCenters: Array<Centro> = [];
+  center: any;
 
   // variables modal preview report
-  name = "old name";
+  name = 'old name';
   showIt = false;
   //
 
-  constructor(private fb: FormBuilder, private router: Router, private admin: AdminService, private cdr: ChangeDetectorRef, private data : DataService) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private admin: AdminService,
+    private cdr: ChangeDetectorRef,
+    private data: DataService,
+    private routeActiva: ActivatedRoute
+  ) {
     this.formAdd = fb.group({
       nombre: ['', Validators.required],
       centros: ['', Validators.required],
@@ -49,22 +61,25 @@ export class AddModReportComponent implements OnInit {
       hasta: ['', Validators.required],
       axe: [],
       variable: [],
-      deliverdate: ['', Validators.required]
+      deliverdate: ['', Validators.required],
     });
   }
 
-  ngOnInit(): void {
+  ngOnInit(){
     this.setFlagAddEdit(false);
-    this.getCenters()
+    this.getCenters();
+    this.getDataFromRute()
+    this.getReportByID()
+    this.getAxes();
+    this.getVariables();
     // console.log(this.arrayAxes)
-
   }
 
   // modal preview report
   showModal() {
     this.showIt = true;
-    this.formAdd.value.axe = this.arrayAxes
-    this.formAdd.value.variable = this.arrayVaribles
+    this.formAdd.value.axe = this.arrayAxes;
+    this.formAdd.value.variable = this.arrayVaribles;
   }
 
   closeModal(newName: string) {
@@ -72,42 +87,45 @@ export class AddModReportComponent implements OnInit {
     if (newName) this.name = newName;
   }
   //
-//  guarda axes de componente selects
-  storageAxes(axes: any,idComponent: number ){
+  //  guarda axes de componente selects
+  storageAxes(axes: any, idComponent: number) {
     this.arrayAxes[idComponent] = axes;
     // console.log("axes", this.arrayAxes)
     // console.log(idComponent)
   }
   // guarda array variables de componente selects
-  storageVariables( variablesArray: any,idComponent: number ){
+  storageVariables(variablesArray: any, idComponent: number) {
     this.arrayVaribles[idComponent] = variablesArray;
     // console.log("variables", this.arrayVaribles)
   }
-  // 
-  // agrega un elemento al arreglo de selects y tambien a axes y variables 
+  //
+  // agrega un elemento al arreglo de selects y tambien a axes y variables
   createEje() {
-    this.arrayc.push(this.arrayc.length + 1)
-    this.arrayAxes.push(1)
-    this.arrayVaribles.push(1)
+    this.arrayc.push(this.arrayc.length + 1);
+    this.arrayAxes.push(1);
+    this.arrayVaribles.push(1);
   }
 
   catchCenter(e: any) {
     this.center = e;
+    this.centerSelect()
   }
-
 
   removeVariable(variable: any) {
     this.variables = this.variables.filter((res: any) => res !== variable);
   }
 
-  confirm(datos:Report) {
-    this.data.arrayAxes = this.arrayAxes
-    this.data.arrayVariables = this.arrayVaribles
-    this.data.arrayCenters = this.formAdd.value.centros
-    
+  confirm(datos: Report) {
+    this.data.arrayAxes = this.arrayAxes;
+    this.data.arrayVariables = this.arrayVaribles;
+    this.data.arrayCenters = this.formAdd.value.centros;
+
     // console.log('report',datos)
     this.setFlagAddEdit(true);
-    this.router.navigate(['admin/dashboard/reportes/creacion-de-reportes/add-mod-report/preview-report',datos])
+    this.router.navigate([
+      'admin/dashboard/reportes/creacion-de-reportes/add-mod-report/preview-report',
+      datos,
+    ]);
   }
 
   getCenters() {
@@ -124,25 +142,98 @@ export class AddModReportComponent implements OnInit {
     });
   }
 
-
-    // Para modal de advertencia de cambio de pantalla
-    setFlagAddEdit(value: boolean) {
-      this.flagAddEdit = value;
-      localStorage.setItem('flagAddEdit', JSON.stringify(this.flagAddEdit));
-    }
-    onSelection($event: any) {
-      // console.log($event);
-      this.showDialog = false;
-      if ($event === 'ok') {
-        this.subject.next(true);
-        this.setFlagAddEdit(false);
-      } else {
-        this.subject.next(false);
-      }
-    }
-    openDialog() {
-      // console.log('opn dialog');
-      this.showDialog = true;
-    }
-    //
+  // Para modal de advertencia de cambio de pantalla
+  setFlagAddEdit(value: boolean) {
+    this.flagAddEdit = value;
+    localStorage.setItem('flagAddEdit', JSON.stringify(this.flagAddEdit));
   }
+  onSelection($event: any) {
+    // console.log($event);
+    this.showDialog = false;
+    if ($event === 'ok') {
+      this.subject.next(true);
+      this.setFlagAddEdit(false);
+    } else {
+      this.subject.next(false);
+    }
+  }
+  openDialog() {
+    // console.log('opn dialog');
+    this.showDialog = true;
+  }
+  //
+
+  getDataFromRute() {
+    this.routeActiva.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get("report")
+    });
+    console.log(this.id)
+    if(this.id){
+      this.getReportByID()
+    }
+    
+  }
+
+  getReportByID(){
+    this.admin.getReportById(this.id).subscribe({
+      next: (data) => {
+        setTimeout(() => this.cdr.detectChanges());
+        this.report = data;
+        this.nombre = this.report.nombre
+        this.desde = this.report.periodoDesde
+        this.hasta = this.report.periodoHasta
+        this.deliverdate = this.report.fechaEntrega
+        console.log(this.nombre);
+      },
+      error: (err) => {
+        setTimeout(() => this.cdr.detectChanges());
+        // console.log(err);
+      },
+    });
+
+   
+  }
+
+  
+
+
+  getAxes() {
+    this.admin.getAxes().subscribe({
+      next: (data: axes[]) => {
+        this.listOfAxes = data;
+        setTimeout(() => this.cdr.detectChanges());
+        // console.log(this.listOfAxes);
+      },
+      error: (err) => {
+        // console.log(err);
+        if (err.status === 401) {
+          this.router.navigate(['/auth']);
+        }
+      },
+      complete: () => {
+        // console.log('Request get axes complete');
+      },
+    });
+  }
+
+  getVariables() {
+    this.admin.getVariables().subscribe({
+      next: (data: variable[]) => {
+        this.listOfVariables = data;
+        setTimeout(() => this.cdr.detectChanges());
+        // console.log(this.listOfVariables);
+      },
+      error: (err) => {
+        // console.log(err);
+        if (err.status === 401) {
+          this.router.navigate(['/auth']);
+        }
+      },
+      complete: () => {
+        // console.log('Request get axes complete');
+      },
+    });
+  }
+
+
+}
