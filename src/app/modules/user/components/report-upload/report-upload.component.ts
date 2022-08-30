deeeeb46bf8f41e6f536e190aab707aedadd9edf
gabrielrios27 <juangrios27@gmail.com>
2022-08-30 12:49:49 -0300

    Feat: Crea vista  de finalizar reporte
    Con un flag de lastAxe se determina si se muestra esta vista que le permite al usuario confirmar el reporte para enviarlo a la ong

--- src/app/modules/user/components/report-upload/report-upload.component.ts
+++ src/app/modules/user/components/report-upload/report-upload.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { variable } from 'src/app/modules/admin/models';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserService } from '../../services';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-report-upload',
@@ -44,10 +47,10 @@ export class ReportUploadComponent implements OnInit {
       variables: [
         {
           id: 21,
-          nombre: 'Cantidad de participantes en taller1',
-          descripcion: 'desc.222',
-          tipo: 'Textual',
-          genero: 'false',
+          nombre: 'Cantidad de participantes en talleres',
+          descripcion: 'Taller 1 y talller 2',
+          tipo: 'Numérico',
+          genero: 'true',
           escala_valor: 'false',
           valor_inicial: 'null',
           valor_final: 'null',
@@ -62,12 +65,28 @@ export class ReportUploadComponent implements OnInit {
       complete: false,
     },
     {
-      axe: 'Acompañamiento Educativo2',
+      axe: 'SEGURIDAD NUTRICIONAL',
       variables: [
         {
           id: 54,
-          nombre: 'Cantidad de participantes en taller3222222',
-          descripcion: 'desc.222444',
+          nombre: 'Cantidad de comidas dadas anualmente por persona',
+          descripcion: 'desayuno, merienda, almuerzo, cena',
+          tipo: 'Numérico',
+          genero: 'false',
+          escala_valor: 'false',
+          valor_inicial: 'null',
+          valor_final: 'null',
+          etiqueta_inicial: 'null',
+          etiqueta_final: 'null',
+          eje: {
+            id: 8,
+            nombre: 'SEGURIDAD NUTRICIONAL',
+          },
+        },
+        {
+          id: 54,
+          nombre: 'Cantidad de participantes',
+          descripcion: 'En todos los comedores',
           tipo: 'Numérico',
           genero: 'true',
           escala_valor: 'false',
@@ -77,35 +96,35 @@ export class ReportUploadComponent implements OnInit {
           etiqueta_final: 'null',
           eje: {
             id: 8,
-            nombre: 'Acompañamiento Educativo2',
+            nombre: 'SEGURIDAD NUTRICIONAL',
           },
         },
       ],
       complete: false,
     },
     {
-      axe: 'Acompañamiento Educativo333',
+      axe: 'ACOMPAÑAMIENTO EN SALUD',
       variables: [
         {
           id: 71,
-          nombre: 'cant. talleres33333',
-          descripcion: 'desc',
+          nombre: 'Calidad de la intervención en salud',
+          descripcion: '',
           tipo: 'Textual',
           genero: 'false',
           escala_valor: 'true',
-          valor_inicial: '0',
+          valor_inicial: '1',
           valor_final: '5',
-          etiqueta_inicial: 'Malo',
-          etiqueta_final: 'Muy Bueno',
+          etiqueta_inicial: 'MUY MALO',
+          etiqueta_final: 'EXCELENTE',
           eje: {
             id: 5,
-            nombre: 'Acompañamiento Educativo3',
+            nombre: 'ACOMPAÑAMIENTO EN SALUD',
           },
         },
         {
           id: 72,
-          nombre: 'Cantidad de participantes en taller1333333',
-          descripcion: 'desc.222',
+          nombre: 'Descripcion de eventos realizados',
+          descripcion: 'En las diferentes localidades',
           tipo: 'Textual',
           genero: 'false',
           escala_valor: 'false',
@@ -115,13 +134,13 @@ export class ReportUploadComponent implements OnInit {
           etiqueta_final: 'null',
           eje: {
             id: 5,
-            nombre: 'Acompañamiento Educativo3',
+            nombre: 'ACOMPAÑAMIENTO EN SALUD',
           },
         },
         {
           id: 73,
-          nombre: 'cantidad 233333',
-          descripcion: 'desc',
+          nombre: 'Cantidad de acompañamientos/intervenciones',
+          descripcion: 'vacunas, ESI, anticoncepción, atención directa',
           tipo: 'Numérico',
           genero: 'false',
           escala_valor: 'false',
@@ -131,23 +150,7 @@ export class ReportUploadComponent implements OnInit {
           etiqueta_final: 'null',
           eje: {
             id: 5,
-            nombre: 'Acompañamiento Educativo3',
-          },
-        },
-        {
-          id: 74,
-          nombre: 'Cantidad de participantes en taller333333',
-          descripcion: 'desc.222444',
-          tipo: 'Numérico',
-          genero: 'true',
-          escala_valor: 'false',
-          valor_inicial: 'null',
-          valor_final: 'null',
-          etiqueta_inicial: 'null',
-          etiqueta_final: 'null',
-          eje: {
-            id: 8,
-            nombre: 'Acompañamiento Educativo3',
+            nombre: 'ACOMPAÑAMIENTO EN SALUD',
           },
         },
       ],
@@ -157,7 +160,12 @@ export class ReportUploadComponent implements OnInit {
 
   @Output() reportToUpload = new EventEmitter<any>();
   @Output() flagBtnGoBack = new EventEmitter<boolean>();
+  @Output() flagLastAxeEmit = new EventEmitter<boolean>();
   @Input('idReport') idReport: number = 0;
+  @Input('flagLastAxe') flagLastAxe: boolean = false;
+  //para recibir click en el btn guardar y salir del comp. upload report
+  clickSaveExitSubscription: Subscription;
+  nameReport: string = 'C00010';
   axeToUpload: any;
   variablesReport: variable[] = {} as variable[];
   variablesToUpload: any[] = [];
@@ -165,15 +173,28 @@ export class ReportUploadComponent implements OnInit {
   reportPartial: any[] = [];
   flagNoVariable: boolean = false;
   indexOfVariables: number = 0;
-  flagLastAxe: boolean = false;
-  constructor(private router: Router) {}
+  indexOfAxe: number = 0;
+
+  constructor(
+    private router: Router,
+    private userSvc: UserService,
+    private viewportScr: ViewportScroller
+  ) {
+    this.clickSaveExitSubscription = this.userSvc
+      .getClickSaveExit()
+      .subscribe(() => {
+        this.onSaveExit();
+      });
+  }
 
   ngOnInit(): void {
     this.axeToShow();
     this.createBiAlphabet();
   }
   axeToShow() {
+    this.indexOfAxe = 0;
     for (let item of this.report) {
+      this.indexOfAxe++;
       if (!item.complete) {
         this.axeToUpload = item.axe;
         this.variablesReport = item.variables;
@@ -190,6 +211,7 @@ export class ReportUploadComponent implements OnInit {
     }
   }
   confirmCompleteAxe() {
+    this.viewportScr.scrollToPosition([0, 0]);
     let i = 0;
     for (let item of this.report) {
       i++;
@@ -200,6 +222,7 @@ export class ReportUploadComponent implements OnInit {
     }
     if (i === this.report.length) {
       this.flagLastAxe = true;
+      this.flagLastAxeEmit.next(this.flagLastAxe);
     }
   }
   getVariablesToUpload($event: any) {
@@ -226,6 +249,8 @@ export class ReportUploadComponent implements OnInit {
   }
   getVariablesToSaveExit($event: any) {
     this.variablesToUpload.push($event);
+    console.log(this.variablesToUpload);
+
     if (this.variablesToUpload.length === this.variablesReport.length) {
       for (let item of this.variablesToUpload) {
         if (item) {
@@ -266,6 +291,11 @@ export class ReportUploadComponent implements OnInit {
           i++;
         }
       }
+    }
+  }
+  onSaveExit() {
+    if (this.flagLastAxe) {
+      this.router.navigate(['/user/dashboard/mis-reportes/pendientes']);
     }
   }
 }
