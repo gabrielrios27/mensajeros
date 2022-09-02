33e7cc92c361742d19151cb176d793e2b275d8db
MatiSal <salinasmatias100@gmail.com>
2022-09-02 11:20:00 -0300

    Feat: agrega funcion axeAsig
    esta funcion asigna elementos a los arryay de variables y ejes para pasarlos al componente selects

--- src/app/modules/admin/containers/selecs-axes-variables/selecs-axes-variables.component.ts
+++ src/app/modules/admin/containers/selecs-axes-variables/selecs-axes-variables.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { axes, variable } from '../../models';
 import { AdminService } from '../../services/admin.service';
@@ -6,94 +13,118 @@ import { AdminService } from '../../services/admin.service';
 @Component({
   selector: 'app-selecs-axes-variables',
   templateUrl: './selecs-axes-variables.component.html',
-  styleUrls: ['./selecs-axes-variables.component.scss']
+  styleUrls: ['./selecs-axes-variables.component.scss'],
 })
 export class SelecsAxesVariablesComponent implements OnInit {
   @Output() axes = new EventEmitter<any>();
   @Output() variablesArray = new EventEmitter<any>();
-  @Input() arrayAxes : any
-  @Input() arrayVariables: Array<any> = []
-  
-  variables: any
-  axe: any
+  @Input() arrayAxes: any;
+  @Input() arrayVariables: any;
 
-  listOfAxes: Array<axes> = []
-  listOfVariables: Array<variable> = []
-  listOfVariablesShow: Array<variable> = []
+  variables: any;
+  axe: any;
 
-  constructor(private router: Router, private admin: AdminService, private cdr: ChangeDetectorRef) { }
+  listOfAxes: Array<axes> = [];
+  listOfVariables: Array<variable> = [];
+  listOfVariablesShow: Array<variable> = [];
+
+  constructor(
+    private router: Router,
+    private admin: AdminService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.getAxes()
-    this.getVariables()
-    console.log(this.arrayAxes)
+    this.getAxes();
+    this.getVariables();
+    this.axeAsig()
   }
   capturarVariables(e: any) {
     this.variables = e;
-    this.variablesArray.emit(this.variables)
+    this.variablesArray.emit(this.variables);
   }
 
   capturarEje(e: any) {
     // this.axe = this.arrayAxes
-    this.axe = e
+    this.axe = e;
     // filter variables per axe
-    this.listOfVariablesShow = this.listOfVariables.filter((res:any)=>{
-      return res.eje.id == this.axe.id
-    })
-    // 
-    console.log("arrays", this.arrayAxes)
+    this.listOfVariablesShow = this.listOfVariables.filter((res: any) => {
+      return res.eje.id === this.axe.id;
+    });
+    //
+    // console.log("arrays", this.arrayAxes)
     this.axes.emit(this.axe);
   }
 
   removeVariable(variable: any) {
-    this.variablesArray.emit(this.variables)
+    this.variablesArray.emit(this.variables);
     this.variables = this.variables.filter((res: any) => res !== variable);
   }
 
-  axeAsig(){
-    this.axe = this.listOfAxes.find((res:any)=> res.id == this.arrayAxes.id)
-    console.log(this.axe)
+  // this function assigns the variables and the axes and displays on the screen 
+  axeAsig() {
+    if (this.arrayAxes != null) {
+      
+      this.axe = this.listOfAxes.find(
+        (res: any) => res.id == this.arrayAxes.id
+      );
+      this.listOfVariablesShow = this.arrayVariables.filter((res: any) => {
+        return res.eje.id == this.axe.id;
+      });
+      this.variables = ( this.arrayVariables.filter((res: any) => {
+        return res.eje.id == this.axe.id;
+      }));
+
+      console.log("variablesss",this.variables);
+      console.log(this.axe);
+    }
+  }
+  // 
+  variableAsig() {
+    if (this.arrayVariables) {
+      this.variables = this.arrayVariables;
+      // console.log('variables', this.variables);
+    }
   }
 
   getAxes() {
-    this.admin
-      .getAxes()
-      .subscribe({
-        next: (data: axes[]) => {
-          this.listOfAxes = data;
-          setTimeout(() => this.cdr.detectChanges());
-          // console.log(this.listOfAxes);
-          this.axeAsig()
-          // this.axe = this.arrayAxes
-        },
-        error: (err) => {
-          // console.log(err);
-          if (err.status === 401) {
-            this.router.navigate(['/auth']);
-          }
-        },
-        complete: () => {
-          // console.log('Request get axes complete');
-        },
-      });
+    this.admin.getAxes().subscribe({
+      next: (data: axes[]) => {
+        this.listOfAxes = data;
+        setTimeout(() => this.cdr.detectChanges());
+        // console.log(this.listOfAxes);
+        this.axeAsig();
+        // this.axe = this.arrayAxes
+      },
+      error: (err) => {
+        // console.log(err);
+        if (err.status === 401) {
+          this.router.navigate(['/auth']);
+        }
+      },
+      complete: () => {
+        // console.log('Request get axes complete');
+      },
+    });
   }
 
-  getVariables(){
-    this.admin.getVariables().subscribe({ next: (data: variable[]) => {
-      this.listOfVariables = data;
-      setTimeout(() => this.cdr.detectChanges());
-      // console.log(this.listOfVariables);
-    },
-    error: (err) => {
-      // console.log(err);
-      if (err.status === 401) {
-        this.router.navigate(['/auth']);
-      }
-    },
-    complete: () => {
-      // console.log('Request get axes complete');
-    },
-  });
+  getVariables() {
+    this.admin.getVariables().subscribe({
+      next: (data: variable[]) => {
+        setTimeout(() => this.cdr.detectChanges());
+        this.listOfVariables = data;
+        // this.variableAsig();
+        // console.log(this.listOfVariables);
+      },
+      error: (err) => {
+        // console.log(err);
+        if (err.status === 401) {
+          this.router.navigate(['/auth']);
+        }
+      },
+      complete: () => {
+        // console.log('Request get axes complete');
+      },
+    });
   }
-
 }
