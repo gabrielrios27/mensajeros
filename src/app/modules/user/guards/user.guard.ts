eb15e8fbfe71fb265fda1c2d78c62f1b463b658f
MatiSal <salinasmatias100@gmail.com>
2022-09-07 12:33:22 -0300

    Feat: crea guard de usuario

--- src/app/modules/user/guards/user.guard.ts
+++ src/app/modules/user/guards/user.guard.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { Observable, of, map } from 'rxjs';
+import { AuthService } from '../../auth/services';
 
 @Injectable()
 export class UserGuard implements CanActivate {
+    constructor(private _authSvc: AuthService, private router: Router) { }
     canActivate(): Observable<boolean> {
-        return of(true);
+        let isUserLocalStorage;
+        let isUserLocalStorageJSON = localStorage.getItem('isUser');
+        if (isUserLocalStorageJSON) {
+            isUserLocalStorage = JSON.parse(isUserLocalStorageJSON);
+            if (isUserLocalStorage) {
+                return of(true);
+            }
+        }
+        let isUser = this._authSvc.getRole().pipe(
+            map((data: any) => {
+                if (data.authority === 'ROLE_USER') {
+                    localStorage.setItem('isUser', JSON.stringify(true));
+                    return true;
+                } else {
+                    localStorage.removeItem('isUser');
+                    this.router.navigate(['/auth']);
+                    return false;
+                }
+            })
+        );
+        return isUser;
     }
 }
