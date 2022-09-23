d0a6c265f042366987be5c1f307df7257502c09e
gabrielrios27 <juangrios27@gmail.com>
2022-09-23 18:34:40 -0300

    Feat: Crea componente para crear informe comparativo

---
+++ src/app/modules/admin/containers/compare-reports/compare-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compare-reports',
+  templateUrl: './compare-reports.component.html',
+  styleUrls: ['./compare-reports.component.scss']
+})
+export class CompareReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
