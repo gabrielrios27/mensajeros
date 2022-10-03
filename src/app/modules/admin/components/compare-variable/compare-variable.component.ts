b5a410acd5e1cec8972407810a1f68507d4c36b1
Juan Gabriel Rios D'Amico <juangrios27@gmail.com>
2022-10-03 16:36:51 -0300

    Feat: Maqueta variables textuales de escala de valor

--- src/app/modules/admin/components/compare-variable/compare-variable.component.ts
+++ src/app/modules/admin/components/compare-variable/compare-variable.component.ts
@@ -8,7 +8,20 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CompareVariableComponent implements OnInit {
   @Input('variable') variable: any = {};
   @Input('index') index: string = 'A';
+  valueScale: number[] = [];
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createValueScale();
+  }
+  createValueScale() {
+    if (this.variable.escala_valor === 'true') {
+      let lastValue: number = Number(this.variable.valor_final);
+      let firstValue: number = Number(this.variable.valor_inicial);
+      this.valueScale = [];
+      for (let i = firstValue; i <= lastValue; i++) {
+        this.valueScale.push(i);
+      }
+    }
+  }
 }
